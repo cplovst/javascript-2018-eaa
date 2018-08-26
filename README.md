07132f5981119964481ebe9dfc713b70b3d4a6fc
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-08-26 20:42:22 +0200

    object multiple videos

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 * Log into your github profile
 * Fork this repo
+* Clone ... and liftoff!
 
 Then you are able to modify the code and upload your work.
 
--- video/video.js
+++ video/video.js
@@ -2,12 +2,18 @@
 /**
  * file: video.js
  * purpose: display and control video via Js
-
- Note the shorthand selector via the tag's id
  **/
-vid.src = 'https://ia800300.us.archive.org/3/items/bb_snow_white/bb_snow_white_512kb.mp4';
-vid.style.width = '100%'; // ye olde mobile first trick
-vid.style.maxWidth = '800px'; // limit the trick above
-vid.style.height = 'auto';
-// vid.play(); // ... or better: don't do this!
-vid.controls = true;
+let MyVideo = function () {
+  this.vid.src = 'https://ia800300.us.archive.org/3/items/bb_snow_white/bb_snow_white_512kb.mp4';
+  this.vid.style.width = '100%'; // ye olde mobile first trick
+  this.vid.style.maxWidth = '800px'; // limit the trick above
+  this.vid.style.height = 'auto';
+  // vid.play(); // ... or better: don't do this!
+  this.vid.controls = true;
+}
+
+
+
+
+// af DOM load run the myVideo function
+window.addEventListener('load', MyVideo);
